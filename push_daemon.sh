14b927c22236889c78796673d99be766b6e3db12
root <root@satellite6.lab24.co>
2016-07-19 22:09:00 +0000

    Touch-ups

--- push_daemon.sh
+++ push_daemon.sh
@@ -6,7 +6,7 @@
 WATCHDIR=/opt/test
 TRANSDIR=/opt/test/transfer
 BASEURL="https://10.0.93.8/file/satelite6/"
-MAX_SIZE=400M
+MAX_SIZE=1K
 
 if [ $(find $WATCHDIR -maxdepth 1 -type f -size +1c | wc -l) -eq 0 ]
 then
@@ -17,16 +17,14 @@ fi
 # grab the first file in that list
 THEFILE=$(find $WATCHDIR -maxdepth 1 -type f | head -1)
 
-echo split -d -b ${MAX_SIZE} ${THEFILE} ${THEFILE}.
-split -d -b ${MAX_SIZE} ${THEFILE} ${THEFILE}.
+# split the file up, keeping track of what's created
+SPLITLIST=$(split -a2 -d -b ${MAX_SIZE} ${THEFILE} ${THEFILE}. --verbose | cut -d' ' -f3 | sed s/\’\//g | sed s/\‘//g )
 
 # count the number of files split created
-TOTALCNT=$(ls ${THEFILE}.?? | wc -l)
+TOTALCNT=$(echo ${SPLITLIST}.?? | wc -w)
 TOTALCNT=$(( ${TOTALCNT} - 1 ))
 
-ls -lha $WATCHDIR
-
-for file in $(ls ${THEFILE}.??)
+for file in ${SPLITLIST[@]}
 do
   TRANSFILE=${TRANSDIR}/$(basename ${file}).transfer
   if [ -f ${TRANSFILE} ]
@@ -39,9 +37,14 @@ do
   sha256sum ${file} >> ${file}.txt
   echo >> ${file}.txt
   cat ${file} | base64 >> ${file}.txt
+# remove file part (pre-conversion)
   rm ${file}
   echo Uploading Part ${file}
-  curl -k -f -T ${file}.txt ${BASEURL}$(basename ${file})_of_${TOTALCNT}.txt > $TRANSFILE
-  rm ${file}.txt
+#  curl -k -f -T ${file}.txt ${BASEURL}$(basename ${file})_of_${TOTALCNT}.txt > $TRANSFILE
+#  remove file conversion
+#  rm ${file}.txt
+#  just a test mv for now:
+   mv ${file}.txt /opt/test/$(basename ${file})_of_${TOTALCNT}.txt
 done
+# remove original from WATCHDIR
 rm $THEFILE
