74362c4d4a8163c409640fd549c29b2cece0079e
Taylor Biggs <taylor@redhat.com>
2016-08-04 14:41:34 -0400

    Add final touches

--- normalize.sh
+++ normalize.sh
@@ -35,26 +35,29 @@ do
   else
     # make sure all parts are available, otherwise go on to next file
     TOTALCNT=$(echo ${THEFILE} | rev | cut -d. -f2 | cut -d_ -f1 | rev)
+    declare -i OKCNT=0
     for num in $(seq -f %02g 0 ${TOTALCNT})
     do
       FILE=${FILENONUM}.${num}_of_${TOTALCNT}.txt
       BASENAME=$(basename $FILE | rev | cut -d. -f3- | rev)
       if ! [ -f ${FILE} ] 
       then
-        echo "$FILE has not yet been transferred, skipping this group for now."
+        echo "$(date '+%Y%m%d - %H:%M:%S') "$FILE has not yet been transferred, skipping this group for now."
         break
       else
+        OKCNT=$OKCNT+1
+      fi
+      if [ $OKCNT -eq $(($TOTALCNT + 1)) ]
+      then
         break 2
       fi
-      echo file check loop done with $FILE
     done
   fi
-echo big loop done with $FILENONUM
 done
 
 touch $TMPDIR/${FILENONUM}.inprogress
 
-echo $THEFILE found.  Starting convert/check routine.
+echo "$(date '+%Y%m%d - %H:%M:%S') $THEFILE found.  Starting convert/check routine."
 
 # /opt/test/something.0_of_12.txt
 # figure out how many parts we're dealing with
@@ -68,15 +71,15 @@ for num in $(seq -f %02g 0 ${TOTALCNT})
 do
   FILE=${FILENONUM}.${num}_of_${TOTALCNT}.txt
   BASENAME=$(basename $FILE | rev | cut -d. -f3- | rev)
-  echo Converting $FILE to $DONEDIR/$BASENAME.$num
+  echo "$(date '+%Y%m%d - %H:%M:%S') Converting $FILE to $DONEDIR/$BASENAME.$num"
   tail -n +3 $FILE | base64 -d > $DONEDIR/$BASENAME.$num
-  echo Checking $DONEDIR/$BASENAME.$num
+  echo "$(date '+%Y%m%d - %H:%M:%S')  Checking $DONEDIR/$BASENAME.$num"
   echo $(head -2 $FILE | tail -1 | cut -d' ' -f1) $DONEDIR/$BASENAME.$num > $DONEDIR/$BASENAME.$num.sha
   sha256sum -c $DONEDIR/$BASENAME.$num.sha
   retval=$?
   if [ $retval -ne 0 ]
   then
-    echo SHA256 returned error, quitting!
+    echo "$(date '+%Y%m%d - %H:%M:%S') SHA256 returned error, quitting!"
     exit 1
   else
     rm $FILE
