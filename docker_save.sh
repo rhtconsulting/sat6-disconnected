97e4befa5bb8d9f0b5f94811e0ee1e6461b5fd5a
Taylor Biggs <taylor@redhat.com>
2017-03-28 21:01:31 -0400

    Rewrite docker-save to be more reliable - use image IDs instead of date strings.

--- docker_save.sh
+++ docker_save.sh
@@ -1,16 +1,18 @@
 #!/bin/bash -x
 
-STORAGE=/var/lib/pulp/exports/docker
+STORAGE=/var/lib/portkey/docker
+SENT_IMAGES=/var/lib/portkey/sent-images.txt
 IMAGE_STORAGE=${STORAGE}/images
-DESTINATION=/var/lib/pulp/exports/to_send/
+DESTINATION=/var/lib/portkey/to_send/
+DOCKER_IMAGES=$(mktemp)
 
-repos=$(/var/lib/pulp/exports/docker_list_images.py | egrep  '^rhel|^openshift3|^rhscl/')
+repos=$(/var/lib/portkey/sat6-disconnected/docker_list_images.py | egrep  '^rhel|^openshift3|^rhscl/|^jboss|^dotnet' | grep -v beta)
 
 # do a fresh pull of everything in our regex above
 for r in $repos;
 do
   SAVE_DIR=$(dirname ${IMAGE_STORAGE}/$r)
-  sudo mkdir -p ${SAVE_DIR}
+  mkdir -p ${SAVE_DIR}
   CLEANEXIT=1
   TRIES=1
   while [ ${CLEANEXIT} -ne 0 -a ${TRIES} -lt 4 ]; 
@@ -21,13 +23,25 @@ do
   done
 done
 
-# we only want to save and push stuff changed within the past week...
-for r in $(docker images | grep 'days ago' | cut -d' ' -f1 | grep registry.access.redhat.com | sort | uniq | sed s?registry.access.redhat.com/??)
-do
-  VERSIONSTRING=$(docker images | grep $r | awk '{print $1 ":" $2}'| sort | uniq)
 
-  echo docker save $VERSIONSTRING \| gzip \> ${IMAGE_STORAGE}/${r}.tar.gz
-  docker save $VERSIONSTRING | gzip > ${IMAGE_STORAGE}/${r}.tar.gz
-  underscore=$(echo ${r} | sed s^/^_^g)
-  mv -v ${IMAGE_STORAGE}/${r}.tar.gz ${DESTINATION}/${underscore}.tar.gz.docker
+# Gen up current list of images
+docker images > $DOCKER_IMAGES
+
+# Save only those image IDs that we haven't saved before
+for image in $(cat $DOCKER_IMAGES | awk '{print $3}' | sort | uniq)
+do
+  if [[ $( grep ${image} ${SENT_IMAGES} ) == '' ]]
+  then 
+    VERSIONSTRING=$(cat $DOCKER_IMAGES | grep ${image} | awk '{print $1 ":" $2}'| sort | uniq)
+    
+    echo docker save $VERSIONSTRING \| gzip \> ${IMAGE_STORAGE}/${image}.tar.gz
+    docker save $VERSIONSTRING | gzip > ${IMAGE_STORAGE}/${image}.tar.gz
+    mv -v ${IMAGE_STORAGE}/${image}.tar.gz ${DESTINATION}/${image}.tar.gz.docker
+    temp_sort=$(mktemp)
+    echo ${image} | sort ${SENT_IMAGES} - > ${temp_sort}
+    mv -f ${temp_sort} ${SENT_IMAGES}
+  fi
 done
+
+# toss the new mapping into to_send, to double-check versions on the other side
+mv -v $DOCKER_IMAGES ${DESTINATION}/docker-image-mapping.txt
