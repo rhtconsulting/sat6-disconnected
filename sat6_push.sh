3334a35bf7df4bc2e872822652af016fcb2109b4
Taylor Biggs <taylor@redhat.com>
2017-07-13 15:27:05 -0400

    Update Sat6 Push script with Kickstart logic

--- sat6_push.sh
+++ sat6_push.sh
@@ -4,20 +4,29 @@
 # a script to perform a "hammer export" of everything available on this (local) satellite
 
 TARFILE=/var/lib/portkey/temp/$(date +%Y%m%d)-CDNincr.tar
-#INCRDIR=/var/lib/pulp/katello-export/Default_Organization-Default_Organization_View-v1.0-incremental/Default_Organization/Library/
 INCRDIR=/var/lib/pulp/katello-export/Default_Organization-Default_Organization_View-v1.0-incremental/
 METADIR=/var/lib/pulp/published/yum/https/
 DESTDIR=/var/lib/portkey/to_send
+# set whether to download kickstart info (only necessary for the first sync of KS repos)
+DL_KICKSTARTS=0
 
-# first export the entire Sat6 Library since a few days ago:
-#rm -rf $INCRDIR
-HAMMERTIME="/usr/bin/hammer --password password content-view version export --content-view-id 1 --organization-id 1 --since $(date -d 'last week' +%Y-%m-%dT00:00:00)"
+# first wait for anything currently synchronizing on the Satellite
+WAIT=$(/usr/bin/hammer --password password task list --search running | grep Synchronize | wc -l)
+while [[ $WAIT -ne 0 ]]
+do
+  sleep 120
+  WAIT=$(/usr/bin/hammer --password password task list --search running | grep Synchronize | wc -l)
+done
+
+# now export the entire Sat6 Library since a few days ago:
+rm -rf $INCRDIR
+HAMMERTIME="/usr/bin/hammer --password password content-view version export --content-view-id 1 --organization-id 1 --since $(date -d '2 days ago' +%Y-%m-%dT00:00:00)"
 echo $HAMMERTIME
 $HAMMERTIME
 
 # download correct updateinfo metadata
-find $INCRDIR -name repodata -exec rm -rfv {} \; 2>/dev/null
-for REPO in $(curl -sk --cert /root/.pulp/user-cert.pem https://satellite6.local.host/pulp/api/v2/repositories/ | jq .[].id |sed s^\"^^g | tail )
+find $INCRDIR -name repodata -exec rm -rf {} \; 2>/dev/null
+for REPO in $(curl -sk --cert /root/.pulp/user-cert.pem https://satellite6.local.host/pulp/api/v2/repositories/ | jq .[].id |sed s^\"^^g | grep -v Images)
 do
   FEED=$(curl -sk --cert /root/.pulp/user-cert.pem https://satellite6.local.host/pulp/api/v2/repositories/${REPO}/importers/yum_importer/ | jq .config.feed | sed s^\"^^g)
   if [ $FEED == 'null' ]
@@ -29,6 +38,7 @@ do
     /var/lib/pulp/importers/$REPO-yum_importer/pki/ca.crt --inet4-only -r --no-host-directories --quiet --directory-prefix=$INCRDIR/Default_Organization/Library/  \
     -A repomd.xml $FEED/repodata/repomd.xml
   echo repo $REPO
+  echo feed $FEED
   FILES=$(cat $INCRDIR/Default_Organization/Library/$FPATH/repodata/repomd.xml | grep href | sed -e s/'^.*<location href=\"repodata\/'// | sed -e s/'\"\/>.*$'//)
   for file in $FILES
   do
@@ -36,12 +46,87 @@ do
       /var/lib/pulp/importers/$REPO-yum_importer/pki/ca.crt --inet4-only -r --no-host-directories --quiet --directory-prefix=$INCRDIR/Default_Organization/Library/  \
       $FEED/repodata/${file}
   done
+
+  # for special cases (kickstarts), download addons repodata...
+  if [ $DL_KICKSTARTS -ne 0 ]
+  then
+    if [ $(echo ${FEED} | grep kickstart | grep '6/6' | grep server)n != 'n' ]
+    then
+      for addon in HighAvailability ScalableFileSystem LoadBalancer Server ResilientStorage
+      do
+        wget -N --certificate=/var/lib/pulp/importers/$REPO-yum_importer/pki/client.crt --private-key=/var/lib/pulp/importers/$REPO-yum_importer/pki/client.key --ca-certificate \
+        /var/lib/pulp/importers/$REPO-yum_importer/pki/ca.crt --inet4-only -r --no-host-directories --quiet --directory-prefix=$INCRDIR/Default_Organization/Library/  \
+        -A "repomd.xml,.treeinfo,productid" $FEED/${addon}/repodata/
+        FILES=$(cat $INCRDIR/Default_Organization/Library/${FPATH}/${addon}/repodata/repomd.xml | grep href | sed -e s/'^.*<location href=\"repodata\/'// | sed -e s/'\"\/>.*$'//)
+        for file in $FILES
+        do
+          wget -N --certificate=/var/lib/pulp/importers/$REPO-yum_importer/pki/client.crt --private-key=/var/lib/pulp/importers/$REPO-yum_importer/pki/client.key --ca-certificate \
+          /var/lib/pulp/importers/$REPO-yum_importer/pki/ca.crt --inet4-only -r --no-host-directories --quiet --directory-prefix=$INCRDIR/Default_Organization/Library/  \
+          ${FEED}/${addon}/repodata/${file}
+        done
+      done
+    fi
+    if [ $(echo ${FEED} | grep kickstart | grep '6/6' | grep workstation)n != 'n' ]
+    then
+      for addon in ScalableFileSystem Workstation
+      do
+        wget -N --certificate=/var/lib/pulp/importers/$REPO-yum_importer/pki/client.crt --private-key=/var/lib/pulp/importers/$REPO-yum_importer/pki/client.key --ca-certificate \
+        /var/lib/pulp/importers/$REPO-yum_importer/pki/ca.crt --inet4-only -r --no-host-directories --quiet --directory-prefix=$INCRDIR/Default_Organization/Library/  \
+        -A "repomd.xml,.treeinfo,productid" $FEED/${addon}/repodata/
+        FILES=$(cat $INCRDIR/Default_Organization/Library/${FPATH}/${addon}/repodata/repomd.xml | grep href | sed -e s/'^.*<location href=\"repodata\/'// | sed -e s/'\"\/>.*$'//)
+        for file in $FILES
+        do
+          wget -N --certificate=/var/lib/pulp/importers/$REPO-yum_importer/pki/client.crt --private-key=/var/lib/pulp/importers/$REPO-yum_importer/pki/client.key --ca-certificate \
+          /var/lib/pulp/importers/$REPO-yum_importer/pki/ca.crt --inet4-only -r --no-host-directories --quiet --directory-prefix=$INCRDIR/Default_Organization/Library/  \
+          ${FEED}/${addon}/repodata/${file}
+        done
+      done
+    fi
+    if [ $(echo ${FEED} | grep kickstart | grep '7/7' | egrep 'server|system-z')n != 'n' ]
+    then
+      for addon in HighAvailability ResilientStorage
+      do
+        wget -N --certificate=/var/lib/pulp/importers/$REPO-yum_importer/pki/client.crt --private-key=/var/lib/pulp/importers/$REPO-yum_importer/pki/client.key --ca-certificate \
+        /var/lib/pulp/importers/$REPO-yum_importer/pki/ca.crt --inet4-only -r --no-host-directories --quiet --directory-prefix=$INCRDIR/Default_Organization/Library/  \
+        -A "repomd.xml,.treeinfo,productid" $FEED/addons/${addon}/repodata/
+        FILES=$(cat $INCRDIR/Default_Organization/Library/${FPATH}/addons/${addon}/repodata/repomd.xml | grep href | sed -e s/'^.*<location href=\"repodata\/'// | sed -e s/'\"\/>.*$'//)
+        for file in $FILES
+        do
+          wget -N --certificate=/var/lib/pulp/importers/$REPO-yum_importer/pki/client.crt --private-key=/var/lib/pulp/importers/$REPO-yum_importer/pki/client.key --ca-certificate \
+          /var/lib/pulp/importers/$REPO-yum_importer/pki/ca.crt --inet4-only -r --no-host-directories --quiet --directory-prefix=$INCRDIR/Default_Organization/Library/  \
+          ${FEED}/addons/${addon}/repodata/${file}
+        done
+      done
+    fi
+  else
+    find $INCRDIR -name kickstart -type d -exec rm -rf {} \; 2>/dev/null
+  fi
 done
 
-chown -R apache.apache $INCRDIR
+
+# add-in ostree data
+find -L /var/lib/pulp/published/ostree/web/ -type f | sort > /tmp/os_tree.txt
+if [[ $(diff /var/lib/portkey/os_tree.txt /tmp/os_tree.txt) != '' ]]
+then
+  for osdir in $(find -L /var/lib/pulp/published/ostree/web/ -type d )
+  do
+    dir=$(echo ${osdir} | cut -d/ -f 8-)
+    if  ! [[ -d ${INCRDIR}/${dir} ]]
+    then
+      mkdir -p ${INCRDIR}/${dir}
+    fi
+  done
+  for osfile in $(find -L /var/lib/pulp/published/ostree/web/ -mtime -2 -type f)
+  do
+    cp -f ${osfile} ${INCRDIR}/$(echo ${osfile} | cut -d/ -f 8-)
+  done
+  cp -f /tmp/os_tree.txt /var/lib/portkey/os_tree.txt
+fi
+
+chown -R foreman.foreman $INCRDIR
 
 # tar everything up
-TARCMD="/usr/bin/tar -cvf ${TARFILE} -C ${INCRDIR} ."
+TARCMD="/usr/bin/tar -cf ${TARFILE} -C ${INCRDIR} ."
 echo $TARCMD
 $TARCMD
 
