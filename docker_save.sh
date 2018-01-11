e5624bd3d76eed5a492bb5747bdf5f752c8c51fc
Taylor Biggs <taylor@redhat.com>
2018-01-11 12:55:46 -0500

    Update docker_push to add "redhat" namespace, plus Crunchy stuff

--- docker_save.sh
+++ docker_save.sh
@@ -6,10 +6,11 @@ IMAGE_STORAGE=${STORAGE}/images
 DESTINATION=/var/lib/portkey/to_send/
 DOCKER_IMAGES=$(mktemp)
 
-repos=$(/var/lib/portkey/sat6-disconnected/docker_list_images.py | egrep  '^rhel|^openshift3|^rhscl/|^jboss|^dotnet|^cloudforms' | grep -v beta)
+rh_repos=$(/var/lib/portkey/sat6-disconnected/docker_list_images.py | egrep  '^rhel|^openshift3|^rhscl/|^jboss|^dotnet|^cloudforms|^redhat' | grep -v beta)
+crunch_repos=$(curl https://access.crunchydata.com/containerinfo/containerversions.txt | grep -v ^# | cut -d: -f1)
 
 # do a fresh pull of everything in our regex above
-for r in $repos;
+for r in $rh_repos;
 do
   SAVE_DIR=$(dirname ${IMAGE_STORAGE}/$r)
   mkdir -p ${SAVE_DIR}
@@ -23,6 +24,19 @@ do
   done
 done
 
+for r in $crunch_repos;
+do
+  SAVE_DIR=$(dirname ${IMAGE_STORAGE}/$r)
+  mkdir -p ${SAVE_DIR}
+  CLEANEXIT=1
+  TRIES=1
+  while [ ${CLEANEXIT} -ne 0 -a ${TRIES} -lt 4 ]; 
+  do
+    docker pull -a registry.crunchydata.com/$r
+    CLEANEXIT=$?
+    ((TRIES++))
+  done
+done
 
 # Gen up current list of images
 docker images > $DOCKER_IMAGES
